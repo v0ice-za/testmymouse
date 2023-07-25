457ed48af845f0304513309a1a78100424e8af3a
Michael Bell <michael.bell0509@gmail.com>
2023-07-25 22:03:34 +0200

    Slider game changes

--- pages/games/slider-game.js
+++ pages/games/slider-game.js
@@ -1,27 +1,54 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import Grid from "@mui/material/Grid";
 import Slider from "@mui/material/Slider";
 
 const SliderGame = () => {
 
     const [value, setValue] = useState(50);
+    const [height, setHeight] = useState('200px');
+    const [width, setWidth] = useState('15px');
+    const [orientation, setOrientation] = useState('vertical');
+    const [y, setY] = useState(0);
+    const [x, setX] = useState(0);
+
+    const move = () => {
+        setX(Math.floor(Math.random() * (window.outerWidth/2)));
+        setY(Math.floor(Math.random() * (window.innerHeight/2)));
+        if(Math.random() > 0.5){
+            setOrientation('vertical');
+            setHeight('200px');
+            setWidth('15px');
+        } else {
+            setOrientation('horizontal');
+            setHeight('15px');
+            setWidth('200px');
+        }
+    }
 
     const handleScroll = (event) => {
+
         const direction = event.deltaY > 0 ? -10 : 10;
         const newValue = value + direction;
         if (newValue >= 0 && newValue <= 100) {
             setValue(newValue);
         }
+        if(value === 0 || value === 100){
+            setValue(50);
+            setHeight('200px');
+            move();
+        }
+
     };
 
 
     return (
         <>
             <Grid container>
-                <Grid item sx={{height: '400px', width: '200px'}}>
+                <Grid item sx={{height: height, width: '200px'}}>
                     <Slider
-                        orientation={'vertical'}
+                        orientation={orientation}
                         value={value}
+                        sx={{position: 'absolute', top: y, left: x, height: height, width: width}}
                         min={0}
                         max={100}
                         onWheel={handleScroll}
